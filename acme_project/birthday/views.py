a6b37875227a1106b104a050027fd45f274cd219
polyalyameow <117765092+polyalyameow@users.noreply.github.com>
2024-10-25 01:56:51 +0200

    Add CBV

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,33 +1,46 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
 from .models import Birthday
 from .utils import calculate_birthday_countdown
 
-def birthday(request, pk=None):
-    if pk is not None:
-        instance = get_object_or_404(Birthday, pk=pk)
-    else:
-        instance = None
-    form = BirthdayForm(request.POST or None, files=request.FILES or None, instance=instance)
-    context = {'form': form}
-    if form.is_valid():
-        form.save()
-        birthday_countdown = calculate_birthday_countdown(
-            form.cleaned_data['birthday']
-        )
-        context.update({'birthday_countdown': birthday_countdown})
-        return redirect('birthday:list')
-    return render(request, 'birthday/birthday.html', context=context)
+class BirthdayCreateView(CreateView):
+    model = Birthday
+    form_class = BirthdayForm
+    template_name = "birthday/birthday.html"
+    success_url = reverse_lazy('birthday:list')
 
-def birthday_list(request):
-    birthdays = Birthday.objects.all().order_by('id')
-    paginator = Paginator(birthdays, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {'page_obj': page_obj}
-    return render(request, 'birthday/birthday_list.html', context)
+# def birthday(request, pk=None):
+#     if pk is not None:
+#         instance = get_object_or_404(Birthday, pk=pk)
+#     else:
+#         instance = None
+#     form = BirthdayForm(request.POST or None, files=request.FILES or None, instance=instance)
+#     context = {'form': form}
+#     if form.is_valid():
+#         form.save()
+#         birthday_countdown = calculate_birthday_countdown(
+#             form.cleaned_data['birthday']
+#         )
+#         context.update({'birthday_countdown': birthday_countdown})
+#         return redirect('birthday:list')
+#     return render(request, 'birthday/birthday.html', context=context)
+
+# def birthday_list(request):
+#     birthdays = Birthday.objects.all().order_by('id')
+#     paginator = Paginator(birthdays, 10)
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+#     context = {'page_obj': page_obj}
+#     return render(request, 'birthday/birthday_list.html', context)
+
+class BirthdayListView(ListView):
+    model = Birthday
+    ordering = 'id'
+    paginate_by = 10
 
 def delete_birthday(request, pk):
     instance = get_object_or_404(Birthday, pk=pk)
