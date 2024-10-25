2e47f93af9168d34d8db1da5b0ec91f38a0e406e
polyalyameow <117765092+polyalyameow@users.noreply.github.com>
2024-10-25 02:23:44 +0200

    Add new CBVs

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
 from .models import Birthday
 from .utils import calculate_birthday_countdown
 
-class BirthdayCreateView(CreateView):
+class BirthdayMixin:
     model = Birthday
+    success_url = reverse_lazy('birthday:list')
+
+class BirthdayFormMixin:
     form_class = BirthdayForm
     template_name = "birthday/birthday.html"
-    success_url = reverse_lazy('birthday:list')
+
+class BirthdayCreateView(BirthdayMixin, CreateView):
+    pass
+
+class BirthdayUpdateView(BirthdayMixin, UpdateView):
+    pass
 
 # def birthday(request, pk=None):
 #     if pk is not None:
@@ -42,12 +50,16 @@ class BirthdayListView(ListView):
     ordering = 'id'
     paginate_by = 10
 
-def delete_birthday(request, pk):
-    instance = get_object_or_404(Birthday, pk=pk)
-    form = BirthdayForm(instance=instance)
-    context = {'form': form}
-    if request.method == 'POST':
-        instance.delete()
-        return redirect('birthday:list')
-    return render(request, 'birthday/birthday.html', context)
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
+    pass
+
+
+# def delete_birthday(request, pk):
+#     instance = get_object_or_404(Birthday, pk=pk)
+#     form = BirthdayForm(instance=instance)
+#     context = {'form': form}
+#     if request.method == 'POST':
+#         instance.delete()
+#         return redirect('birthday:list')
+#     return render(request, 'birthday/birthday.html', context)
 
